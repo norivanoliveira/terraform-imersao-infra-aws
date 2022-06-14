06ba42e354bf2f22704b2d89968c04e6d0f8a61c
Norivan Oliveira Silva <norivan.oliveira@gmail.com>
2022-06-14 20:50:59 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+  key_name = "Iac"
+
+  tags = {
+    Name = "MeuPrimeiroEC2ComTF"
+  }
+}
